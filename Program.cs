290430bb1c0f7ae425723bb82982bbe4ef764b46
TaHuC <TaHuC@hotmail.com>
2017-01-27 10:28:10 +0200

    Finish bool

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BooleanVariable
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			bool getString = bool.Parse(Console.ReadLine());
+
+			if (getString == true)
+			{
+				Console.WriteLine("Yes");
+			}
+			else
+			{
+				Console.WriteLine("No");
+			}
+		}
+	}
+}
